4ae592c3788ac88d4aa234535b399240ec116562
przem <przemyslawkazimierczak@gmail.com>
2023-06-28 23:57:01 +0200

    Zmiana koloru naglowka

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end(`<html><body><h1>This is SailboutShop</h1></body></html>`);
+<h1 style="color:blue;">This is a Blue Heading</h1>
 }
 
 const server = http.createServer(requestListener);
