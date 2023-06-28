a664ee4217a2e39b0a19fe5ab03c5ddde34b4d68
przem <przemyslawkazimierczak@gmail.com>
2023-06-29 00:05:30 +0200

    kolejne próba z kolorem

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`<html><body><h1>This is SailboutShop</h1><h1 style="color:blue;"></body></html>`);
+  res.end(`<html><body><h1>This is SailboutShop</h1><h1 style="color: blue;"></body></html>`);
 }
 
 const server = http.createServer(requestListener);
