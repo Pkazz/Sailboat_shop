fc521db4e9580181f74a16e5e6395cb10a12dd49
przem <przemyslawkazimierczak@gmail.com>
2023-06-28 23:49:04 +0200

    Poprawki w kodzie

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.endres.end(`<html><body><h1>This is HTML</h1></body></html>`);
+  res.end(`<html><body><h1>This is HTML</h1></body></html>`);
 }
 
 const server = http.createServer(requestListener);
